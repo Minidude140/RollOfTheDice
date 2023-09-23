d13c73a2b82c925d7a614b3a45384dc76b386103
Zac <zac.minidude140@Gmail.com>
2023-09-23 12:29:37 -0600

    Add project files.

---
+++ RollOfTheDice/Module1.vb
@@ -0,0 +1,17 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Roll of the Dice
+'https://github.com/Minidude140
+
+
+Option Explicit On
+Option Strict On
+
+Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
