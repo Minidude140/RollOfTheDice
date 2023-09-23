d460fa2adaaa70be790b9436d8a1d75ddbd3df24
Zac <zac.minidude140@Gmail.com>
2023-09-23 12:46:34 -0600

    Added TODO list and declared some variables

--- RollOfTheDice/RollOfTheDice.vb
+++ RollOfTheDice/RollOfTheDice.vb
@@ -11,10 +11,25 @@
 'Header row--> the roll result numbers 2 To 12
 'Data row --> the total count Of how many times Each number came up
 
+'TODO
+'[]Create a function to Pull random number 1-6
+'[]Call function twice and assign to diceOne and diceTwo
+'[]Add two dice rolls together to diceTotal
+'[]create an array diceRollTally() where first row is possible roll totals {2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12}
+'[]second row of diceRollTally() should be the number of times the diceTotal equals the corresponding column
+'[]compare diceTotal to the possible rolls and increment corresponding array element
+'[]repeat 1000 times and record totals in diceRollTally() array
+'[]we might want do separate variables for each total and add them all to the array after the 1000 rolls
+
 Option Explicit On
 Option Strict On
 
 Module RollOfTheDice
+
+    Dim diceOne As Integer
+    Dim diceTwo As Integer
+    Dim diceTotal As Integer
+
 
     Sub Main()
 
