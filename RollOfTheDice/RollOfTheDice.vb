46c1dd4e590ed73690bf496b4f3b7c0877ccb34d
Zac <zac.minidude140@Gmail.com>
2023-09-23 12:32:28 -0600

    Renamed, Added Git Link, and added assignment instructions

---
+++ RollOfTheDice/RollOfTheDice.vb
@@ -0,0 +1,23 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Roll of the Dice
+'https://github.com/Minidude140/RollOfTheDice.git
+
+'ASSIGNMENT INSTRUCTIONS
+'Write a program that rolls two six-sided dice 1,000 times And accumulates the count Of Each number rolled efficiently In an array.
+'Display the results In the console In formatted columns With headers
+
+'Header row--> the roll result numbers 2 To 12
+'Data row --> the total count Of how many times Each number came up
+
+Option Explicit On
+Option Strict On
+
+Module RollOfTheDice
+
+    Sub Main()
+
+    End Sub
+
+End Module
